b35d0ae61e1229d9140a87e185992fa83f951868
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2024-09-08 04:11:18 -0700

    Add files via upload

---
+++ keep_alive.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Minecraft bot is running\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+        
